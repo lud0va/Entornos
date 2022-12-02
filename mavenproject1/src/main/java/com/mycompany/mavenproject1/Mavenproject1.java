c8e53014be8cdc54e6b9a078d2ca2add1965d0e7
1dam <ovando.decena.luis@gmail.com>
2022-12-02 12:47:43 +0100

    añadir intellij

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -13,5 +13,6 @@ public class Mavenproject1 {
 
     public static void main(String[] args) {
         System.out.println("Hello World! desde Netbeans");
+        System.out.println("Hello world! desde intellij");
     }
 }
